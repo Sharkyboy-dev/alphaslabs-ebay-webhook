8ecc8a379b90888dc50d99108d88a7bbf6e3a287
SHARKY LABS <dev@sharkyboy.com>
2025-04-23 10:04:38 -0400

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,1 +1,20 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+# Must be 32–80 chars
+VERIFICATION_TOKEN = "alphaslabs_notify_token_v1_2025_abcd1234efgh5678ijkl9012mnop3456qrst"
+
+@app.route("/", methods=["GET"])
+def index():
+    return "✅ AlphaSlabs Webhook Online", 200
+
+@app.route("/alphaslabs-notify", methods=["POST"])
+def notify():
+    token = request.headers.get("x-ebay-signature", "")
+    if token.strip() == VERIFICATION_TOKEN:
+        print("✅ Verified eBay Notification")
+        return "OK", 200
+    print("❌ Verification Failed: ", token)
+    return "Forbidden", 403
 
